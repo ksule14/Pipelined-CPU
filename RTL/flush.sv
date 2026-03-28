c74dfa71db66a6aa8cd5436e790d72d16ed795de
Krish <ksule14005@gmail.com>
2026-03-28 19:31:13 -0400

    cleaned rtl files

--- RTL/flush.sv
+++ RTL/flush.sv
@@ -15,17 +15,12 @@ module flush_controller (
     output logic flush_IF_ID,
     output logic flush_ID_EX,
     output logic pc_redirect,
-    output logic [WORD_WIDTH-1:0] pc_corrected
 );
-    logic [WORD_WIDTH-1:0] branch_pc_plus4;
     logic mispredicted;
 
-    assign branch_pc_plus4 = branch_addr + 4;
-
-    assign mispredicted = branch_resolved &&(branch_taken != predicted_taken);
+    assign mispredicted = branch_resolved && (branch_taken != predicted_taken);
     assign flush_IF_ID = mispredicted;
     assign flush_ID_EX = mispredicted;
     assign pc_redirect = mispredicted;
-    assign pc_corrected = branch_taken ? branch_target : branch_pc_plus4;
 
 endmodule
--- RTL/pc.sv
+++ RTL/pc.sv
@@ -4,12 +4,12 @@ import codes_pkg::DATA_WIDTH;
 module pc (
     input logic clk,
     input logic rst_n,
-    input logic pc_src,
+    input logic pc_redirect,
     input logic [DATA_WIDTH-1:0] pc_branch,
     output logic [DATA_WIDTH-1:0] pc_current
 );
     logic [DATA_WIDTH-1:0] pc_next;
-    assign pc_next = pc_src ? pc_branch : (pc_current + DATA_WIDTH'd4);
+    assign pc_next = pc_redirect ? pc_branch : (pc_current + DATA_WIDTH'd4);
     always_ff @(posedge clk or negedge rst_n) begin
         if (!rst_n) begin
             pc_current <= DATA_WIDTH'd0; // Reset PC to 0
