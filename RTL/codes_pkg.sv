dd56bf007dbe7a3a822f091fb30fc1f55d62410a
Krish <ksule14005@gmail.com>
2026-03-28 21:36:41 -0400

    Switched to 32 bit CPU

--- RTL/codes_pkg.sv
+++ RTL/codes_pkg.sv
@@ -3,7 +3,7 @@ typedef enum logic [2:0] {
     ADD, SUB, AND, OR, SLT, SLTU // ALU control signals
 } alu_control;
 
-parameter DATA_WIDTH = 64;
+parameter DATA_WIDTH = 32;
 parameter WORD_WIDTH = 32;
 parameter DEPTH = 256;
 endpackage
