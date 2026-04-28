808239171aac48a89f5bb25468d75505c9547362
Krish <ksule14005@gmail.com>
2026-04-28 12:19:09 -0400

    commented forwarding unit

--- RTL/forwarding_u.sv
+++ RTL/forwarding_u.sv
@@ -1,26 +1,32 @@
-
+// forwarding unit detects read-after-write data hazards and selects the most recent value for each alu operand
+// by forwarding results from later pipeline stages back to EX, bypassing the register file. Prevents stalls in most cases
+// except in load-use hazards, where it defers to the stalling unit
 module forwarding_u #(parameter REG_WIDTH = 5) (
-    // Source registers for instruction currently in EX.
+    // IDs of the registers currently in EX stage that could require forwarded data
     input logic [REG_WIDTH-1:0] ex_rs1,
     input logic [REG_WIDTH-1:0] ex_rs2,
 
-    // Destination registers of older instructions in MEM and WB.
+    // IDs of destination registers currently in MEM stage (1 ahead of EX) and WB stage (2 ahead of EX)
     input logic [REG_WIDTH-1:0] rd_mem,
     input logic [REG_WIDTH-1:0] rd_wb,
 
-    // Write intent for MEM/WB producers.
+    // high when MEM-stage instruction actually writes a register.
+    // Prevents forwarding when MEM instruction isn't a producer (store/branch).
     input logic reg_write_mem,
 
-    // High when MEM-stage producer is a load (data not ready for EX/MEM bypass).
-    input logic ex_mem_is_load,
+    // same purpose but for WB stage
     input logic reg_write_wb,
+
+    // High when MEM stage instruction is load. Load result comes uot of data memory at the end of MEM stage
+    // cannot be forwarded at EX/MEM boundary because it is not ready yet.
+    input logic ex_mem_is_load,
 
     // Forwarding select encoding:
     // 2'b00 = use register file value
     // 2'b10 = forward from EX/MEM
     // 2'b01 = forward from MEM/WB
-    output logic[1:0] forward_a,
-    output logic[1:0] forward_b
+    output logic[1:0] forward_a, // mux select for rs1 fed into ALU
+    output logic[1:0] forward_b // mux select for rs2 fed into ALU
 );
 
     always_comb begin
@@ -29,8 +35,12 @@ module forwarding_u #(parameter REG_WIDTH = 5) (
 
         // EX/MEM has priority over WB for newest-data wins,
         // except loads, which must be taken from MEM/WB.
+        
+        // forwarding unit cannot forward load-use hazards in the MEM stage because the data is not ready.
+        // This means the pipeline must stall in this instance. Forwarding unit treats load-use hazard in WB stage
+        // as a read-after-write hazard because the data can simply be forwarded as it is sitting in the WB register
         if (reg_write_mem && !ex_mem_is_load && (rd_mem != '0) && (rd_mem == ex_rs1)) begin
-            forward_a = 2'b10;
+            forward_a = 2'b10; 
         end else if (reg_write_wb && (rd_wb != '0) && (rd_wb == ex_rs1)) begin
             forward_a = 2'b01;
         end
